e83df801e33cb7255bc004c705afb3e20ef27150
Sferia78 <contact@sferia.fr>
2025-11-26 13:55:08 +0100

    fix: add server layouts to force dynamic rendering

---
+++ app/onboarding/layout.tsx
@@ -0,0 +1,11 @@
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+
+export default function OnboardingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
+
